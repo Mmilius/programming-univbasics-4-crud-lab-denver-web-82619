c99ee5c9e5afb54cf40b53d5c9619d5fc55503c6
Martin Milius <martin.milius@gmail.com>
2019-08-07 02:30:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   pets = ["dog", "cat"]
-  pets.unshift("elephant")
+  pets.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
