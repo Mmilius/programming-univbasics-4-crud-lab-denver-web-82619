5f0ee2eb2ca8669b898b1be28876e9ac0f1c1431
Martin Milius <martin.milius@gmail.com>
2019-08-07 02:33:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  cats = ["Taylor", "Bob"]
-  cats.pop("")
+  cats = ["1", "2"]
+  cats.pop("3")
 end
 
 def remove_element_from_start_of_array(array)
