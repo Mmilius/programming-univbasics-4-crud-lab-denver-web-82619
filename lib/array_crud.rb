df98d7152f3c5c58ef182e6b8c2d5c9deefc944b
Martin Milius <martin.milius@gmail.com>
2019-08-07 02:49:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  books = ["bible", "dictionary", "novel"]
+  books [-1]
 end
 
 def update_element_from_index(array, index_number, element)
