aacd3f546676c4fd7e19866cf4ccbe2820505a7c
Martin Milius <martin.milius@gmail.com>
2019-08-07 02:39:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   cats = ["taylor, arrays!"]
-  newcats.pop("arrays!")
+  kitten = cats.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
