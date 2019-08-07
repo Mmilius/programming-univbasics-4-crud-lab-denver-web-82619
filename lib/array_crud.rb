68c99444d922bd03828488ff74772a532f043ccc
Martin Milius <martin.milius@gmail.com>
2019-08-07 02:45:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  dogs = ["spike", "max"]
+  dogs = ["wow", "max"]
   nickname = dogs.shift
   p nickname
 end
